402c6a66d148b2f1cb93cd688d7a9ebf78218dea
John Jesus <john.jesus@raytheon.com>
2015-12-08 10:58:48 -0600

    Initial commit.

---
+++ TrySSH/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrySSH
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hit ENTER to continue");
+            Console.ReadLine();
+        }
+    }
+}
