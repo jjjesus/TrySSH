026aa356be156c81337cc18e619c66c8e4a24b54
John Jesus <john.jesus@raytheon.com>
2015-12-08 11:26:05 -0600

    Initial SshClient.

--- TrySSH/Program.cs
+++ TrySSH/Program.cs
@@ -10,6 +10,9 @@ namespace TrySSH
     {
         static void Main(string[] args)
         {
+            Runner runner = new Runner();
+            string results = runner.Run("192.168.10.19", "jjesus", "passw0rd", "ls");
+            Console.WriteLine(results);
             Console.WriteLine("Hit ENTER to continue");
             Console.ReadLine();
         }
---
+++ TrySSH/Runner.cs
@@ -0,0 +1,25 @@
+﻿using Renci.SshNet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrySSH
+{
+    public class Runner
+    {
+        public string Run(string hostName, string userName, string password, string cmd)
+        {
+            string results = string.Empty;
+            using (SshClient ssh = new SshClient(hostName, userName, password))
+            {
+                ssh.Connect();
+                var result = ssh.RunCommand(cmd);
+                if (result.ExitStatus == 0) results = result.Result;
+                ssh.Disconnect();
+            }
+            return results;
+        }
+    }
+}
